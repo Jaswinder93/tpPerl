f6ddd082210f54d2fe82cac5a1dce2fe4cddea09
jas <jas@kali.jas.com>
2021-05-24 16:26:28 +0200

    commentaire exo 4

--- exo4.pl
+++ exo4.pl
@@ -1,5 +1,6 @@
 #!usr/bin/perl -w
 
+#exo 4 : Trinome : Amiot Tanguy / Chaouche Zineb / Singh Jaswinder
 sub freqApparition{
 	my($fichier)= @_; # $$ : process number
 	open(FH, '<', $fichier) or die $!;
@@ -8,6 +9,7 @@ sub freqApparition{
    		chomp;
    		@mots = split(/ /, $_);
    		chomp(@mots);
+		#pour chaque mots trouver incrémente sa fréquence d'appartion dans la liste
    		foreach $unmot (@mots){
         		if (exists($listUnique{$unmot})){
         	    		$listUnique{$unmot}++;
@@ -26,4 +28,5 @@ sub freqApparition{
 	close(FH);	
 }
 
+#prend en parametre le fichier txt duquel il doit lire le contenu
 freqApparition("fichier.txt");
