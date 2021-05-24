0d3a59d85184030ead45f2ba47b380fac7f86dfd
jas <jas@kali.jas.com>
2021-05-24 15:03:45 +0200

    commentaire exo 1

--- exo1.pl
+++ exo1.pl
@@ -1,5 +1,6 @@
 #!usr/bin/perl
 
+#exo 1 TP  Trinome : Amiot Tanguy | Chaouche Zineb | Singh Jaswinder
 # %ENV les variables d'environnements, sort qui trie par ordre alphabétique
 foreach $key (sort keys %ENV) {
   print "$key = $ENV{$key}\n";
